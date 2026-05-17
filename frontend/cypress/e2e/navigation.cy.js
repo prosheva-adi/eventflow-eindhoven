b1c07ac5b922c38bb8bc3524a0fd3699c694ce03
Prosheva,Adelina A.S. <I549034@student.fontys.nl>
2026-05-17 22:54:38 +0200

    add cypress e2e tests

---
+++ frontend/cypress/e2e/navigation.cy.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+describe('Navigation', () => {
+    it('should load the home page', () => {
+        cy.visit('http://localhost:5173/')
+        cy.url().should('eq', 'http://localhost:5173/')
+    })
+
+    it('should navigate to events page', () => {
+        cy.visit('http://localhost:5173/')
+        cy.visit('http://localhost:5173/events')
+        cy.url().should('include', '/events')
+    })
+
+    it('should navigate to login page', () => {
+        cy.visit('http://localhost:5173/login')
+        cy.contains('Sign in to your account').should('exist')
+    })
+
+    it('should navigate to register page', () => {
+        cy.visit('http://localhost:5173/register')
+        cy.url().should('include', '/register')
+    })
+
+    it('should not show venues in navbar for guest', () => {
+        cy.visit('http://localhost:5173/')
+        cy.get('nav').should('not.contain', 'Venues')
+    })
+
+    it('should show venues in navbar for admin', () => {
+        cy.visit('http://localhost:5173/login')
+        cy.get('input[type="email"]').type('admin@gmail.com')
+        cy.get('input[type="password"]').type('Admin123')
+        cy.get('button[type="submit"]').click()
+        cy.wait(2000)
+        cy.get('nav').should('contain', 'Venues')
+    })
+})
