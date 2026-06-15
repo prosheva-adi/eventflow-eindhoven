c682981fb3748d2d3146244e2d99d731a4f23c0b
Prosheva,Adelina A.S. <I549034@student.fontys.nl>
2026-06-15 14:03:03 +0200

    changed event map

--- frontend/src/components/EventMap.jsx
+++ frontend/src/components/EventMap.jsx
@@ -3,6 +3,22 @@ import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
 
 function EventMap() {
     const [venues, setVenues] = useState([]);
